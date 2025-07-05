7f3de308baa9d5e2d54f91bc00b88d16ae214f26
Abhi <abhishekvelekar792000@gmail.com>
2025-07-05 15:50:08 +0530

    client ping added

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/abhi1060/rpc_client/handler"
 	"github.com/gin-gonic/gin"
@@ -10,8 +11,19 @@ import (
 func main() {
 	router := gin.Default()
 
+	router.GET("/ping", pong)
 	router.GET("/", handler.ListAllhandler)
 	router.POST("/add", handler.AddBook)
 
 	log.Fatal(router.Run(":8080"))
+}
+
+
+func pong(c *gin.Context){
+	c.JSON(
+		http.StatusOK,
+		gin.H{
+			"message" : "Pong",
+		},
+	)
 }
